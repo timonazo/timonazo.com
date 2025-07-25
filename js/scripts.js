e4d9a0db1088123b4132e4500f01a54dd85a5fc7
dev-timonazo <222849827+dev-timonazo@users.noreply.github.com>
2025-07-25 13:25:52 -0400

    Commit 0

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2025 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+document.getElementById('t').setAttribute('href', atob('dGVsOisxMzA3NjU1NjY1NCANCg=='))
